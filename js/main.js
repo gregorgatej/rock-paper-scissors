5784fcf8a2f49c4f46229252bad59327a493c3d7
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-09 06:33:09 +0100

    Change human choice to be inputted via buttons

--- js/main.js
+++ js/main.js
@@ -1,55 +1,62 @@
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let message = "";
-    let finalMessage = "";
-    round = 0;
+let humanScore = 0;
+let computerScore = 0;
+let message = "";
 
-    function getComputerChoice() {
-        random_nr = Math.floor(Math.random() * 3) + 1;
-        switch (random_nr) {
-            case 1:
-                return "rock";
-            case 2:
-                return "paper";
-            case 3:
-                return "scissors";
-        }
+const rockBtn = document.querySelector(".rockBtn");
+const paperBtn = document.querySelector(".paperBtn");
+const scissorsBtn = document.querySelector(".scissorsBtn");
+
+rockBtn.addEventListener("click", () => {
+    computerChoice = getComputerChoice();
+    playRound("rock", computerChoice);
+});
+
+paperBtn.addEventListener("click", () => {
+    computerChoice = getComputerChoice();
+    playRound("paper", computerChoice);
+});
+
+scissorsBtn.addEventListener("click", () => {
+    computerChoice = getComputerChoice();
+    playRound("scissors", computerChoice);
+});
+
+function getComputerChoice() {
+    random_nr = Math.floor(Math.random() * 3) + 1;
+    switch (random_nr) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
     }
-
-    function getHumanChoice() {
-        return prompt("Input one of 'rock', 'paper', 'scissors':").toLowerCase();
-    }
-
-    function playRound(humanChoice, computerChoice) {
-
-        if (humanChoice === "rock" && computerChoice === "scissors"
-         || humanChoice === "paper" && computerChoice === "rock"
-         || humanChoice === "scissors" && computerChoice === "paper"
-        ) {
-            message = `Ỳou win this round! ${humanChoice} beats ${computerChoice}!`;
-            humanScore += 1;
-        } else {
-            message = `The computer wins this round... ${computerChoice} beats ${humanChoice}.`;
-            computerScore += 1;
-        }
-    }
-
-    while(humanScore < 5 && computerScore < 5) {
-        const computerChoice = getComputerChoice();
-        const humanChoice = getHumanChoice();
-        if (humanChoice === computerChoice) {
-            console.log(`It's a tie! ${humanChoice} vs. ${computerChoice}.\nRepeating the current round...`);
-        } else {
-            playRound(humanChoice, computerChoice);
-            console.log(`${message}\nHuman score: ${humanScore}\nComputer score: ${computerScore}`);
-        }
-    }
-
-    finalMessage = humanScore > computerScore ?
-        `Congratulations, you have won the game! The final score was:\nHuman score: ${humanScore}\nComputer score: ${computerScore}` :
-        `Unfortunately you have lost the game :( The final score was:\nHuman score: ${humanScore}\nComputer score: ${computerScore}`
-    console.log(finalMessage);
 }
 
-playGame();
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        message = `It's a tie! ${humanChoice} vs. ${computerChoice}.
+                   \nTry again...`;
+    } else {
+        if (humanChoice === "rock" && computerChoice === "scissors"
+            || humanChoice === "paper" && computerChoice === "rock"
+            || humanChoice === "scissors" && computerChoice === "paper"
+        ) {
+            humanScore += 1;
+            message = `Ỳou win this round! ${humanChoice} beats ${computerChoice}! Current score:
+                           \nHuman score: ${humanScore}\nComputer score: ${computerScore}`;
+        } else {
+            computerScore += 1;
+            message = `The computer wins this round... ${computerChoice} beats ${humanChoice}. Current score:
+                           \nHuman score: ${humanScore}\nComputer score: ${computerScore}`;
+        }
+    }
+    if (humanScore === 5 || computerScore === 5) {
+        message = humanScore > computerScore ?
+            `Congratulations, you have won this round (${humanChoice} beats ${computerChoice}) and the game! The final score was:
+             \nHuman score: ${humanScore}\nComputer score: ${computerScore}` :
+            `Unfortunately you have lost this round (${computerChoice} beats ${humanChoice}) and the game :( The final score was:
+             \nHuman score: ${humanScore}\nComputer score: ${computerScore}`
+    }
+    console.log(message);
+}
